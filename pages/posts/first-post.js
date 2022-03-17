19fa13b9de4066f2685bd496055705635e04181f
Crhistian <crhistiandeoliveira@gmail.com>
2022-03-17 16:04:57 -0300

    modifica first-post

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -18,11 +18,6 @@ export default function FirstPost() {
           onLoad={() => console.log(`script loaded correctly, window.FB has been populated`)}
         />
         <h1>First Post</h1>
-        <h2>
-          <Link href="/">
-            <a>Back to home</a>
-          </Link>
-        </h2>
       </Layout>
     </>
   );
