1f70034a982ec90e34518c1b0099687760ed0187
Crhistian <crhistiandeoliveira@gmail.com>
2022-03-17 16:06:57 -0300

    permite img da api

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ['avatars.githubusercontent.com'],
+  },
+  env: {
+    USER: process.env.USER,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
